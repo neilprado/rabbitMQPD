b033e164b066be055120e30ccf8c4ec5d8d8cffb
neilprado <neiljohn17@gmail.com>
2019-10-14 17:02:54 -0300

    Exercicios rabbitmq

---
+++ src/main/java/Consumidor.java
@@ -0,0 +1,26 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+public class Consumidor {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Consumidor");
+        String NOME_FILA = "filaOlá3";
+
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("localhost");
+        Connection conexao = connectionFactory.newConnection();
+
+        Channel canal = conexao.createChannel();
+        canal.queueDeclare(NOME_FILA, true, false, false, null);
+
+        DeliverCallback callback = (consumerTag, delivery) -> {
+            String mensagem = new String(delivery.getBody());
+            System.out.println("Recebi a mensagem: " + mensagem);
+        };
+
+        canal.basicConsume(NOME_FILA, true, callback, consumerTag -> {});
+        System.out.println("Fazendo outro trabalho...");
+    }
+}
---
+++ src/main/java/Produtor.java
@@ -0,0 +1,22 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class Produtor {
+    public static void main(String[] args) throws Exception{
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("localhost");
+        connectionFactory.setPort(5672);
+
+        String NOME_FILA = "filaOlá3";
+        try (
+                Connection connection = connectionFactory.newConnection();
+                Channel channel = connection.createChannel()){
+            channel.queueDeclare(NOME_FILA, true, false, false, null);
+            String mensagem = "Olá mundo!";
+
+            channel.basicPublish("", NOME_FILA, null, mensagem.getBytes());
+            System.out.println("Enviei mensagem: " + mensagem);
+        }
+    }
+}
